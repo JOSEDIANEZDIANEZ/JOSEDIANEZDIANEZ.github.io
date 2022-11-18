424395a4fefadbe7b0879e2458185ce55c081002
JOSEDIANEZDIANEZ <118521465+JOSEDIANEZDIANEZ@users.noreply.github.com>
2022-11-18 10:13:32 +0100

    initial.commit

---
+++ preguntas.js
@@ -0,0 +1,37 @@
+var preguntas=[
+{
+/*pregunta 0*/
+"pregunta":"¿Quién descubrió America?",
+"respuesta":"Cristóbal colón"
+},
+
+{
+/*pregunta 1*/
+"pregunta":"¿Cuál es la capital de España?",
+"respuesta":"Madrid"
+},
+{
+/*pregunta 2*/
+"pregunta":"¿Cómo se llama la ciudad donde vives?",
+"respuesta":"Sevilla"
+},
+{
+/*pregunta 2*/
+"pregunta":"¿Por qué Alejandro tiene la churra tan chica?",
+"respuesta":"Porque es pequeño"
+},
+{
+/*pregunta 2*/
+"pregunta":"¿Por qué Samuel es tan capuyo?",
+"respuesta":"Porque es como su mamá"
+},
+{
+/*pregunta 2*/
+"pregunta":"¿Cuantos dedos tiene una mano?",
+"respuesta":"Cinco"
+},
+{
+/*pregunta 3*/
+"pregunta":"¿Cómo se llama el satélite de la tierra?",
+"respuesta":"luna"
+}];
